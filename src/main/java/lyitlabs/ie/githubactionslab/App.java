817c4050e7214278ef31260d5249f6bd0b1a3f52
L00162876 <L00162876@student.lyit.ie>
2021-03-07 04:59:31 +0000

    Initial files added

---
+++ src/main/java/lyitlabs/ie/githubactionslab/App.java
@@ -0,0 +1,13 @@
+package lyitlabs.ie.githubactionslab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
